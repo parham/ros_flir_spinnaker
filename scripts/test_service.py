56cd16bf82e244c4d354acb584702f45c7c1fcb4
Parham Nooralishahi <parham.nooralishahi@gmail.com>
2021-06-19 01:34:11 -0400

    update the test script

--- scripts/test_service.py
+++ scripts/test_service.py
@@ -5,7 +5,7 @@ import rospy
 from ros_flir_spinnaker.srv import *
 
 if __name__ == "__main__":
-    rospy.wait_for_service('phm_flir_spinnaker/execute')
+    rospy.wait_for_service('phm/phm_flir_spinnaker/execute')
     try:
         exefunc = rospy.ServiceProxy('phm/phm_flir_spinnaker/execute', ExecuteCommand)
         resp = exefunc('AutoFocus')
